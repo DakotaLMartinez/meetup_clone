053637e4f2d606ee0ef0ad17801d25f8ce606a4c
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-09-29 09:27:26 -0700

    add solution to lesson 6 on Authorization

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,14 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :update, :destroy]
+  before_action :authorize_user, only: [:update, :destroy]
+  
   def index
     events = Event.all.includes(:user_events)
     render json: events, each_serializer: EventIndexSerializer
   end
 
   def show
-    event = Event.find(params[:id])
-    render json: event
+    render json: @event
   end
 
   def create
@@ -19,17 +21,15 @@ class EventsController < ApplicationController
   end
 
   def update
-    event = current_user.events.find(params[:id])
-    if event.update(event_params)
-      render json: event, status: :ok
+    if @event.update(event_params)
+      render json: @event, status: :ok
     else
-      render json: event.errors, status: :unprocessable_entity
+      render json: @event.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    event = current_user.created_events.find(params[:id])
-    event.destroy
+    @event.destroy
     # we'll render the event as json in case we want to enable undo functionality from the frontend.
     render json: event, status: :ok
   end
@@ -38,6 +38,15 @@ class EventsController < ApplicationController
 
   def event_params
     params.permit(:title, :description, :location, :start_time, :end_time, :group_name, :cover_image_url)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def authorize_user
+    user_can_modify = current_user.admin? || @event.user_id == current_user.id
+    return render json: { error: "You don't have permission to perform that action" }, status: :forbidden unless user_can_modify
   end
 
 end
